cc42fc8646ea2c18bd7b260578dc1f269d7d07ea
Tom Bugolya <tom@crazylister.com>
2020-06-27 16:03:31 +0300

    Is_data implementation

--- src/FirstIteration/first_iteration.c
+++ src/FirstIteration/first_iteration.c
@@ -9,7 +9,7 @@ void first_iteration(char * filename, FILE * file){
     const char delimiters[] = " \t\n";
     char line[82];
     char * token;
-
+    errors_exist = False;
     while(fgets(line, sizeof(line), file)){
         symbol_flag = False;
         token_counter = 0;
@@ -21,12 +21,12 @@ void first_iteration(char * filename, FILE * file){
 */
             if (is_a_symbol(token))
                 symbol_flag = True;
-            else if (is_data(token))
-                process_data_line(line, token, symbol_flag);
-            else if (is_entry(token))
-                token = strtok(NULL, delimiters);
-            else if (is_extern(token))
-                process_extern_line(line, token);
+            else if (is_data(token)) {
+                if (is_extern(token))
+                    process_extern_line(line, token);
+                else
+                    process_data_line(line, token, symbol_flag);
+            }
             else if (is_command(token))
                 process_command_line(line, token, symbol_flag);
             else if (is_comment(token))
@@ -39,28 +39,58 @@ void first_iteration(char * filename, FILE * file){
 boolean is_a_symbol(char token[]){
     size_t len = strlen(token);
     if (isalpha(token[0]) && token[len - 1] == ':' ) {
-        if (len >= MAX_SYMBOL_CHARS)
-            fprintf(stderr, "Error: Label name \"%s\" is too long, up to %d characters are allowed\n", token, MAX_SYMBOL_CHARS-1);
+        if (len >= MAX_SYMBOL_CHARS) {
+            fprintf(stderr, "Error: Label name \"%s\" is too long, up to %d characters are allowed\n", token,
+                    MAX_SYMBOL_CHARS - 1);
+            errors_exist = True;
+        }
         else if (!symbol_flag)
             return True;
-        else
-            fprintf(stderr, "Error: Unexpected appearance of an extra label \"%s\", only one label can appear per line\n", token);
+        else {
+            fprintf(stderr,
+                    "Error: Unexpected appearance of an extra label \"%s\", only one label can appear per line\n",
+                    token);
+            errors_exist = True;
+        }
     }
     return False;
 }
 boolean is_data(char token[]){
-    return False;
-}
-boolean is_entry(char token[]){
-    return False;
-}
-boolean is_extern(char token[]){
+    size_t len = strlen(token);
+    char * data_name;
+    data_name = (char *) malloc(len - 1);
+    strncpy(data_name, token + 1, len -1);
+    data_name[len - 1] = '\0';
+    if (token[0] == '.') {
+        if (is_valid_data_name(data_name)) {
+            free(data_name);
+            return True;
+        }
+        else
+            fprintf(stderr, "Error: Unknown data command \"%s\"\n", token);
+    }
+    free(data_name);
     return False;
 }
 boolean is_command(char token[]){
     return False;
 }
 boolean is_comment(char token[]){
+    return False;
+}
+boolean is_valid_data_name(char * data_name){
+    if (
+        strcmp(data_name, "string") == 0 ||
+        strcmp(data_name, "data") == 0 ||
+        strcmp(data_name, "entry") == 0 ||
+        strcmp(data_name, "extern") == 0
+    )
+        return True;
+    return False;
+}
+boolean is_extern(char * data_name){
+    if (strcmp(data_name, "extern") == 0)
+        return True;
     return False;
 }
 void process_data_line(char token[], char* line, boolean is_symbol){
--- src/FirstIteration/first_iteration.h
+++ src/FirstIteration/first_iteration.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #define MAX_SYMBOL_CHARS 32
@@ -6,10 +7,10 @@ typedef enum {False, True} boolean;
 void first_iteration(char *, FILE *);
 boolean is_a_symbol(char *);
 boolean is_data(char *);
-boolean is_entry(char *);
-boolean is_extern(char *);
 boolean is_command(char *);
 boolean is_comment(char *);
+boolean is_valid_data_name(char *);
+boolean is_extern(char *);
 void process_data_line(char[], char*, boolean);
 void process_extern_line(char[], char*);
 void process_command_line(char[], char*, boolean);
